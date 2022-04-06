e30019bff95c2fe023bb5c48e44eb426f9a61435
Dany serfaty <davidserfaty2315@gmail.com>
2022-04-06 20:16:04 +0300

    pioche de cartes

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,18 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styles from'./card.module.css'
 
 class Card extends Component {
     constructor(props) {
         super(props);
-        this.state = {
+        let angle = Math.random() * 90 - 45;
+        let xPos = Math.random() * 40 - 20;
+        let yPos = Math.random() * 40 - 20;
+        this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+    
 
-        }
-
+       
     }
+
+  
    
     render() { 
+       
         return ( 
-            <div className="card">
-                <img src="" alt="" />
+            <div className={styles.card}>
+                { <img src={this.props.card.image} style={{ transform: this._transform }} alt="" /> }
+                
             
             </div>
          );
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,27 +1,39 @@
-import { Axios } from 'axios';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Card from './card';
+
 
 
 class Cards extends Component {
     constructor() {
         super();
         this.state = {
-            book: {},
+            arrCards:[],
+            repDeck: {},
             deck_id: '',
-            
+           
 
         }
     }
 
 
     affiche=()=>{
-        var url = `http://deckofcardsapi.com/api/deck/${this.deck_id}/draw/?count=1`
-        Axios.get(url)
+        var url = `http://deckofcardsapi.com/api/deck/${this.state.deck_id}/draw/?count=1`
+        axios.get(url)
+      .then(res => {
+       
+        var card = res.data.cards[0];
+        console.log( 'card',card);
+        var cloneArr = this.state.arrCards
+        cloneArr.push(card)
+        this.setState({arrCards:cloneArr} );
+      })
+      console.log('arr',this.state.arrCards);
         
     }
 
     componentDidMount() {
-         Axios.get('http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+         axios.get('http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
                 .then(response => {
                  this.setState({
                     book: response.data,
@@ -33,10 +45,15 @@ class Cards extends Component {
 
 
     render() { 
+        
         return ( 
             <>
             <div className="cards">
                 <button onClick={()=>this.affiche()}>recevoir une carte !</button>
+                {this.state.arrCards.map(card => (
+                    <Card key= {card.code} card={card} />
+                    
+                ))} 
                 
                 
             </div>
